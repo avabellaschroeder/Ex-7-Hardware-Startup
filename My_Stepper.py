c6bc0df5324a95bb4d8bc0fced81bda888d58b09
avabella <avabellaschroeder@dpengineering.org>
2024-01-31 11:27:22 -0800

    updating code w screen names, basic classes, changing names of things from reused code

--- My_Stepper.py
+++ My_Stepper.py
@@ -26,20 +26,78 @@ from kivy.animation import Animation
 
 from datetime import datetime
 
+time = datetime
+
 from dpeaDPi.DPiComputer import DPiComputer
 from dpeaDPi.DPiStepper import *
 from time import sleep
 
 
+MIXPANEL_TOKEN = "x"
+MIXPANEL = MixPanel("Project Name", MIXPANEL_TOKEN)
+
+SCREEN_MANAGER = ScreenManager()
+MAIN_SCREEN_NAME = 'stepper'
+
+
+
+# microstepping = 8
+# dpiStepper.setMicrostepping(microstepping)
+
+
+
+class MyStepperGUI(App):
+    """
+    Class to handle running the GUI Application
+    """
+
+    def build(self):
+        """
+        Build the application
+        :return: Kivy Screen Manager instance
+        """
+        return SCREEN_MANAGER
+
+
+Window.clearcolor = (.132, .156, .194, 1)  # White
+
+button_toggle = False
+
+
+
 dpiStepper = DPiStepper()
-
 dpiStepper.setBoardNumber(0)
-
+# initialize stepper
 
 if dpiStepper.initialize() != True:
     print("Communication with the DPiStepper board failed.")
 
 dpiStepper.enableMotors(True)
 
-microstepping = 8
-dpiStepper.setMicrostepping(microstepping)
+
+class StepperScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+
+Builder.load_file('stepper.kv')
+SCREEN_MANAGER.add_widget(StepperScreen(name=MAIN_SCREEN_NAME))
+
+
+def send_event(event_name):
+    """
+    Send an event to MixPanel without properties
+    :param event_name: Name of the event
+    :return: None
+    """
+    global MIXPANEL
+
+    MIXPANEL.set_event_name(event_name)
+    MIXPANEL.send_event()
+
+
+if __name__ == "__stepper__":
+    # send_event("Project Initialized")
+    # Window.fullscreen = 'auto'
+    MyStepperGUI().run()
