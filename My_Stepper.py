0fb81df55bbd8a1a8be2233c5c406377fe638cfd
avabella <avabellaschroeder@dpengineering.org>
2024-02-01 12:05:37 -0800

    adding more code, fixing the imports, can now open window with button that does nothing

--- My_Stepper.py
+++ My_Stepper.py
@@ -1,7 +1,10 @@
-#os.environ['DISPLAY'] = ":0.0"
-#os.environ['KIVY_WINDOW'] = 'egl_rpi'
 import pygame
 import os
+import kivy
+
+
+os.environ['DISPLAY'] = ":0.0"
+os.environ['KIVY_WINDOW'] = 'egl_rpi'
 
 from pidev.Joystick import Joystick
 
@@ -34,15 +37,11 @@ from time import sleep
 
 
 MIXPANEL_TOKEN = "x"
-MIXPANEL = MixPanel("Project Name", MIXPANEL_TOKEN)
+MIXPANEL = MixPanel("My Stepper", MIXPANEL_TOKEN)
 
 SCREEN_MANAGER = ScreenManager()
-MAIN_SCREEN_NAME = 'stepper'
+STEPPER_SCREEN_NAME = 'stepper'
 
-
-
-# microstepping = 8
-# dpiStepper.setMicrostepping(microstepping)
 
 
 
@@ -59,14 +58,15 @@ class MyStepperGUI(App):
         return SCREEN_MANAGER
 
 
+
 Window.clearcolor = (.132, .156, .194, 1)  # White
 
 button_toggle = False
 
-
-
 dpiStepper = DPiStepper()
+
 dpiStepper.setBoardNumber(0)
+
 # initialize stepper
 
 if dpiStepper.initialize() != True:
@@ -75,14 +75,49 @@ if dpiStepper.initialize() != True:
 dpiStepper.enableMotors(True)
 
 
+
 class StepperScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+
+
+    def pressed(self):
+        """
+        Function called on button touch event for button with id: testButton
+        :return: None
+        """
+        print("Callback from MainScreen.pressed()")
+
+    # press_count = NumericProperty(0)
+    # def pressed(self, button):
+    #     """f
+    #     Function called on button touch event for button with id: testButton
+    #     :return: None
+    #     """
+    #     global button_toggle
+    #     self.press_count += 1
+    #
+    #     print("Callback from StepperScreen.pressed()")
+    #
+    #     if button_toggle == False:
+    #         button_toggle = True
+    #         button.text = ''
+    #
+    #     else:
+    #         button_toggle = False
+    #         button.text = 'RACOON'
+
+
+
+# class Motor:
+#     def __init__(self):
+
+
+        # microstepping = 8
+        # dpiStepper.setMicrostepping(microstepping)
 
 
 
 Builder.load_file('stepper.kv')
-SCREEN_MANAGER.add_widget(StepperScreen(name=MAIN_SCREEN_NAME))
+SCREEN_MANAGER.add_widget(StepperScreen(name=STEPPER_SCREEN_NAME))
 
 
 def send_event(event_name):
@@ -97,7 +132,7 @@ def send_event(event_name):
     MIXPANEL.send_event()
 
 
-if __name__ == "__stepper__":
+if __name__ == "__main__":
     # send_event("Project Initialized")
     # Window.fullscreen = 'auto'
     MyStepperGUI().run()
